00fb803ce2d6398681b50894d8f4b4833fed2e15
328506045678 <55800819+328506045678@users.noreply.github.com>
2019-10-23 11:32:25 -0400

    Update script.js
    update

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function binary2hex(){
-var box1 = parseFloat(document.getElementById("box1").value)// setting input box1
-var box2 = parseFloat(document.getElementById("box2").value)// setting input box2
-var box3 = parseFloat(document.getElementById("box3").value)// setting input box3
+    var box1 = parseFloat(document.getElementById("box1").value)// setting input box1
+    var box2 = parseFloat(document.getElementById("box2").value)// setting input box2
+    var box3 = parseFloat(document.getElementById("box3").value)// setting input box3
 
 var box = parseInt(box1, 2).toString(16)// Math for Binary to Hex
 
 document.getElementById('box2').value = box // putting the Hex into the input box2
- var (n = 0; n < hex.length; n += 2)
+ // (n = 0; n < hex.length; n += 2)
 
  
 
